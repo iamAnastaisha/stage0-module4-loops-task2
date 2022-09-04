900993784d3b80437094b09571e573aa1d272ef9
nasty <anastaishastil2002@gmail.com>
2022-09-04 20:54:35 +0300

    Updated

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -8,7 +8,7 @@ public class Multiplication {
                 System.out.println(counter * multiplyByAndToInclusive);
                 counter++;
             }
-        } else {
+        } else if (multiplyByAndToInclusive < 0) {
             while (counter >= multiplyByAndToInclusive) {
                 System.out.println(-counter * multiplyByAndToInclusive);
                 counter--;
